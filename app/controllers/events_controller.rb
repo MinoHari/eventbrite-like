c133b20e95c7ac78636e77890d5fadfe88390b3d
Mino <mino.rakoto94@gmail.com>
2018-05-03 18:01:59 +0300

    mino

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,22 @@
 class EventsController < ApplicationController
     def home
     end
+
+    def show
+        @events = Event.all
+    end
+
+    def create
+        @event = Event.new
+        @event.description= params["description"] 
+        puts @event.description
+
+        @event.date = params["date"]
+        @event.place= params['place']
+        @event.save 
+
+        
+
+        redirect_to "/events/show"
+    end
 end
